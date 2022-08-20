2ec68ac47623016cc9845d02f133d9c2bbd67e60
JayTeeSF <jaytee@jayteesf.com>
2022-08-20 13:01:11 -0700

    Extract borders from lanes -- for future use

--- road.js
+++ road.js
@@ -10,33 +10,51 @@ class Road{
     const infinity = 1000000;
     this.top = -infinity;
     this.bottom = infinity;
+
+    const topLeft = {x: this.left, y: this.top};
+    const topRight = {x: this.right, y: this.top};
+    const bottomLeft = {x: this.left, y: this.bottom};
+    const bottomRight = {x: this.right, y: this.bottom};
+
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight,bottomRight]
+    ];
   }
 
   draw(ctx){
     ctx.lineWidth = 5;
     ctx.strokeStyle="white";
 
-    for(let i = 0; i <= this.laneCount; i++) {
-     
+    /*
+     * avoid drawing the 0th and last line, since
+     * those are borders
+     * use dashed-lines for middle lanes
+     * draw 20 pixels then insert a break of 20
+     */
+    for(let i = 1; i <= this.laneCount - 1; i++) {
       const x=lerp(
         this.left,
         this.right,
         i / this.laneCount //ranges from 0 to 1
       );
 
-      // use dashed-lines for middle lanes
-      if(i>0 && i<this.laneCount) {
-        // draw 20 pixels then insert a break of 20
-        ctx.setLineDash([20,20]);
-      } else {
-        ctx.setLineDash([]);
-      }
+      ctx.setLineDash([20,20]);
 
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
+    // now draw the borders
+    ctx.setLineDash([]);
+    this.borders.forEach(border =>{
+      ctx.beginPath();
+      ctx.moveTo(border[0].x,border[0].y);
+      ctx.lineTo(border[1].x,border[1].y);
+      ctx.stroke();
+    });
   }
 
   getLaneCenter(laneIndex){
