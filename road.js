2c88860a9bad1f4969b6cb81fb5230d1d3d15bec
JayTeeSF <jaytee@jayteesf.com>
2022-08-20 12:40:22 -0700

    Center the car in a lane --no more than the # of lanes available

--- road.js
+++ road.js
@@ -1,5 +1,5 @@
 class Road{
-  constructor(x,width,laneCount=4){
+  constructor(x,width,laneCount=3){
     this.x = x;
     this.width = width;
     this.laneCount = laneCount;
@@ -37,6 +37,23 @@ class Road{
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+  }
+
+  getLaneCenter(laneIndex){
+    const laneWidth = this.width / this.laneCount;
+    // an offset (1/2 the width of a lane)
+    // from the left of any particular lane
+    // could also use lerp and return some percentage..
+    let shouldReturn =  this.left + laneWidth/2 +
+      Math.min(laneIndex, this.laneCount - 1) * laneWidth;
+    /*
+    let percentOfALane = 0.5
+    let lanePercent = (laneIndex + 1) / this.laneCount // lane 1 of 4 is 25%; lane 2 of 4 is 50%, etc...
+    let p = percentOfALane * (lanePercent)
+    let lerpReturn =  lerp(this.left, this.right,p)
+    console.log(`shouldReturn: ${shouldReturn} ?= ${lerpReturn}: lerpReturn`)
+    */
+    return shouldReturn
   }
 
 }
