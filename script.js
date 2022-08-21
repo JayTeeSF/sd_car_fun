771db6d0629e8b81e1321ab1cf0ec4a78a136d43
JayTeeSF <jaytee@jayteesf.com>
2022-08-21 14:52:38 -0700

    generate 1K cars and choose the best brain; use a total of 3 cars as traffic (for now)

--- script.js
+++ script.js
@@ -8,16 +8,33 @@ const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 
 const road = new Road(carCanvas.width/2, carCanvas.width * 0.9);
-const N = 100;
+const N = 1000;
 const cars = generateCars(N);
+let bestCar = cars[0];
+let bestBrain = localStorage.getItem("bestBrain")
+if (bestBrain) {
+  bestCar.brain = JSON.parse(bestBrain)
+}
 //= new Car(road.getLaneCenter(1),100,30,50, "AI");
 
 const traffic = [
   // put a rogue car in our way ...that drives slower
-  new Car(road.getLaneCenter(1),-100,30,50,"ROGUE",2)
+  new Car(road.getLaneCenter(1),-100,30,50,"ROGUE",2),
+  new Car(road.getLaneCenter(0),-1000,30,50,"ROGUE",2),
+  new Car(road.getLaneCenter(3),-500,30,50,"ROGUE",2),
 ]
 
 animate();
+
+function save() {
+  localStorage.setItem(
+    "bestBrain",
+    JSON.stringify(bestCar.brain)
+  );
+}
+function destroy() {
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
   const cars = [];
@@ -34,12 +51,20 @@ function animate(time) {
   for (let i = 0; i < cars.length; i++) {
     cars[i].update(road.borders, traffic);
   }
+  
+  // define best car as the one that makes it the furthest
+  // i.e. has lowest 'y' value
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].y < bestCar.y) {
+      bestCar = cars[i];
+    }
+  }
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
   carCtx.save();
-  carCtx.translate(0, -cars[0].y + carCanvas.height * 0.7);
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
 
   road.draw(carCtx);
   for(let i = 0; i < traffic.length; i++) {
@@ -53,11 +78,11 @@ function animate(time) {
   carCtx.globalAlpha = 1;
   // redraw the first car to emphasize it
   // and only draw its with sensors...
-  cars[0].draw(carCtx, "blue", true);
+  bestCar.draw(carCtx, "blue", true);
 
   carCtx.restore();
 
-  Visualizer.drawNetwork(networkCtx, cars[0].brain);
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
 
   networkCtx.lineDashOffset=-time/50;
   requestAnimationFrame(animate);
